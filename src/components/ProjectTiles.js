e4fbafe09a7b9855db336ef5539e58273c6eab65
Giancarlo Galliani <galliani.08@gmail.com>
2017-04-08 13:06:54 +0100

    Change transition of VenueTiles.

--- src/components/ProjectTiles.js
+++ src/components/ProjectTiles.js
@@ -6,26 +6,27 @@ const ProjectTiles = props => {
   const { projects, currentFilter } = props
   return (
       <div className='projects__tiles'>
-        <TileTransition>
         { projects.reverse().map( (item, index) => {
             let { title, description, imageUrl, tags, linkUrl } = item
+            // Note: the TileTransition is rendered within the map as the animations otherwise appeared janky.
             let tile = 
-            <div key={index} className='projects__tile-container'>
-                <ProjectTile
-                  title={title}
-                  description={description}
-                  imageUrl={imageUrl}
-                  tags={tags}
-                  linkUrl={linkUrl}
-                />
-              </div>
+            <TileTransition>
+              <div key={index} className='projects__tile-container'>
+                  <ProjectTile
+                    title={title}
+                    description={description}
+                    imageUrl={imageUrl}
+                    tags={tags}
+                    linkUrl={linkUrl}
+                  />
+                </div>
+              </TileTransition>
               // Return only tiles that match the current filter. If no filters are set, return all tiles.
              return (
             (( currentFilter && item.tags.indexOf(currentFilter) >= 0) || !currentFilter || currentFilter === 'all' ) && tile
               )
             }
         )}
-        </TileTransition>
       </div>
   )
 }
